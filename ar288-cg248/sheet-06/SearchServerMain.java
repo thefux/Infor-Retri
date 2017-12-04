1e7f941b85dd282665dc594f37369779199e3bde
Chandran Goodchild <ch.goodchild@gmail.com>
2017-12-04 15:36:17 +1000

    Before adding regex

--- ar288-cg248/sheet-06/SearchServerMain.java
+++ ar288-cg248/sheet-06/SearchServerMain.java
@@ -30,6 +30,7 @@ public class SearchServerMain {
     }
     int port = Integer.parseInt(args[0]);
 
+    // Create socket.
     ServerSocket server = new ServerSocket(port);
 
     // Server loop.
@@ -37,46 +38,53 @@ public class SearchServerMain {
       // Wait for client.
       System.out.print("Waiting for query on port " + port + " ... ");
       System.out.flush();
+
       Socket client = server.accept();
       System.out.println("client connected from " + client.getInetAddress());
 
       // Client connected; set read timeout.
       client.setSoTimeout(5000);
 
+
       // Read first line from the request (not enough for ES6).
       BufferedReader input = new BufferedReader(new InputStreamReader(
           client.getInputStream()));
       StringBuilder requestBuilder = new StringBuilder();
-      String fileName;
-      String line = "";
-//      StringBuilder in = new StringBuilder();
-      try {
-        // Redo this as in the lecture!
-        fileName = input.readLine();
+      DataOutputStream output = new DataOutputStream(client.getOutputStream());
 
-//        line = input.readLine();
-//        System.out.println(line);
-//        in.append(input.readLine());
-//        while (!line.equals("") && line != null) {
-//          in.append(input.readLine());
-//          line = input.readLine();
-//        }
-//
-//        fileName = in.toString();
-//        System.out.println(fileName);
+      try {
+        while (true) {
+          // sending the request data in several packets (some of which maybe
+          // empty).
+          String line = input.readLine();
+          if (line.equals("")) {
+            break;
+          }
+          requestBuilder.append(line);
+        }
+
+
+        String fileName = requestBuilder.toString();
+        System.out.println(fileName);
 
         int i = 5;
 
-        if (fileName.equals("")) {
-          fileName = "angryResponse.html";
-          System.out.println("empty string");
-        }
         // When done this way, our server can handle get requests as well as
         // simple requests via telnet...
-        if (fileName.substring(0, 5).equals("GET /")) {
+        if (!(fileName.length() < 1) && fileName.length() >= 5 && fileName
+            .substring(0, 5).equals("GET /")) {
           // It's a browser!
           fileName = fileName.substring(5, fileName.indexOf(" HTTP/1.1"));
+        } else if (fileName.equals("")) {
+          // connecting and not sending anything type 1
+          fileName = "annoyedResponse.html";
+          System.out.println("empty request");
+        } else if (fileName.length() < 5){
+          fileName = "areYouABrowser.html";
         }
+//        else {
+//          throw new IllegalArgumentException();
+//        }
 
         System.out.println(fileName);
 
@@ -117,28 +125,42 @@ public class SearchServerMain {
           //}
         }
 
-          // Send the response.
-          DataOutputStream output = new DataOutputStream(client.getOutputStream());
-          StringBuilder headerBuilder = new StringBuilder();
+        // Send the response.
+        StringBuilder headerBuilder = new StringBuilder();
 
-          headerBuilder.append(statusString + "\r\n");
-          headerBuilder.append("Content-Length: " + contentBytes.length +
-              "\r\n");
-          headerBuilder.append("Content-Type: " + contentType + "\r\n");
-          headerBuilder.append("\r\n");
-          //output.write(contentBytes);
-          output.write(headerBuilder.toString().getBytes("UTF-8"));
-          output.write(contentBytes);
-
-        output.close();
-        input.close();
-        client.close();
+        headerBuilder.append(statusString + "\r\n");
+        headerBuilder.append("Content-Length: " + contentBytes.length +
+            "\r\n");
+        headerBuilder.append("Content-Type: " + contentType + "\r\n");
+        headerBuilder.append("\r\n");
+        //output.write(contentBytes);
+        output.write(headerBuilder.toString().getBytes("UTF-8"));
+        output.write(contentBytes);
 
       } catch (java.net.SocketTimeoutException e) {
-        System.out.println("Read TIMEOUT");
-        input.close();
-        client.close();
+        // connecting and not sending anything type 2
+        // connecting and then aborting type 2
+        String out = "Read TIMEOUT\n";
+        output.write(out.toString().getBytes("UTF-8"));
+        System.out.println(out);
+
+      } catch (NullPointerException e) {
+        // connecting and then aborting type 1
+        String out = "Hey client where are you?!?\n";
+        output.write(out.toString().getBytes("UTF-8"));
+        System.out.println(out);
       }
+//      catch (IllegalArgumentException e) {
+//        String out = "Illegal argument! Try writing something longer and " +
+//            "adding a file type. The file types \".txt\", \".html\" and \"" +
+//            ".css\" are supported.\n";
+//        output.write(out.toString().getBytes("UTF-8"));
+//        System.out.println(out);
+//      }
+
+      output.close();
+      input.close();
+      client.close();
     }
   }
 }
